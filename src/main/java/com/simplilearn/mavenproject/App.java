62bd2459580bfa3e16f9e0f971ba515279a2d7db
Dilan <dilant737@gmail.com>
2022-08-03 12:21:07 -0500

    first commit

---
+++ src/main/java/com/simplilearn/mavenproject/App.java
@@ -0,0 +1,13 @@
+package com.simplilearn.mavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static int addition(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/simplilearn/mavenproject/AppTest.java
@@ -0,0 +1,20 @@
+package com.simplilearn.mavenproject;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testAddition()
+    {
+        assertEquals(3,App.addition(1,2));
+    }
+}
